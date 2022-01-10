df2a14eaf881e1abefedb7a0c4668dcb710b63fb
lky100047 <47895484+lky100047@users.noreply.github.com>
2022-01-10 15:29:24 +0800

    Update app.py

--- morpheus_hf/app.py
+++ morpheus_hf/app.py
@@ -27,3 +27,5 @@ with st.spinner("Discovering Answers.."):
         q_dict = {"question": "In what country is Normandy located?", "id": "56ddde6b9a695914005b9628", "answers": [{"text": "France", "answer_start": 159}, {"text": "France", "answer_start": 159}, {"text": "France", "answer_start": 159}, {"text": "France", "answer_start": 159}], "is_impossible": False}
 
         print(test_morph_qa.morph(q_dict, context))
+        
+        st.write(test_morph_qa.morph(q_dict, context)['context'])
