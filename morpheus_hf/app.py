a105e63c0c4bb4870e6f2b5d043b0e08233ed466
lky100047 <47895484+lky100047@users.noreply.github.com>
2022-01-10 15:23:22 +0800

    Update app.py

--- morpheus_hf/app.py
+++ morpheus_hf/app.py
@@ -10,7 +10,8 @@ def load_qa_model():
     return model
 
 qa = load_qa_model()
-test_morph_qa = MorpheusHuggingfaceQA('deepset/roberta-base-squad2')
+
+
 
 st.title("Ask Questions about your Text")
 sentence = st.text_area('Please paste your article :', height=30)
@@ -21,3 +22,8 @@ with st.spinner("Discovering Answers.."):
         answers = qa(question=question, context=sentence)
         st.write(answers['answer'], answers['Test'])
         
+        test_morph_qa = MorpheusHuggingfaceQA('deepset/roberta-base-squad2')
+        context = "The Normans (Norman: Nourmands; French: Normands; Latin: Normanni) were the people who in the 10th and 11th centuries gave their name to Normandy, a region in France. They were descended from Norse (\"Norman\" comes from \"Norseman\") raiders and pirates from Denmark, Iceland and Norway who, under their leader Rollo, agreed to swear fealty to King Charles III of West Francia. Through generations of assimilation and mixing with the native Frankish and Roman-Gaulish populations, their descendants would gradually merge with the Carolingian-based cultures of West Francia. The distinct cultural and ethnic identity of the Normans emerged initially in the first half of the 10th century, and it continued to evolve over the succeeding centuries."
+        q_dict = {"question": "In what country is Normandy located?", "id": "56ddde6b9a695914005b9628", "answers": [{"text": "France", "answer_start": 159}, {"text": "France", "answer_start": 159}, {"text": "France", "answer_start": 159}, {"text": "France", "answer_start": 159}], "is_impossible": False}
+
+        print(test_morph_qa.morph(q_dict, context))
